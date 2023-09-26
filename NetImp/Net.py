67a2db44d7a0b2d6810d8c33ce118f06d1d154a4
angushushu <angushushu@outlook.com>
2023-09-26 00:01:30 -0400

    net impl

--- NetImp/Net.py
+++ NetImp/Net.py
@@ -1,10 +1,5 @@
-from logging import root
+import itertools
 import networkx as nx
-import matplotlib.pyplot as plt
-from utils import Type
-import utils
-import Model
-from copy import deepcopy
 
 '''
 type:
@@ -25,22 +20,101 @@ function:
     change_weight(node1, node2)
     set_weight(node1, node2)
 '''
+
+
 class Net:
-    def __init__(self, rus: set[str] = set()) -> None:
-        self.id_cnt = 0
+    def __init__(self, unit_nodes=None) -> None:
+        self.net = nx.DiGraph()
+        self.nodes = self.net.nodes
+        self.pos = {} # 可能要为seq添加z轴
+        if unit_nodes:
+            self.net.add_nodes_from(unit_nodes)
+
+    def add_node(self, node):
+        self.net.add_node(node)
+        if isinstance(node, NonUnitNode):
+            for n in node.graph.nodes:
+                self.net.add_edge(n, node, seq=False)
+            for e in node.graph.edges:
+                self.net.add_edge(e[0], node, seq=True)
+                self.net.add_edge(e[1], node, seq=True)
+
+    def add_rep_edge(self, n1, n2):
+        self.net.add_edge(n1, n2, seq=False)
+        n2.add_nodes([n1])
+
+    def add_seq_edge(self, seq, node):
+        self.net.add_edge(seq[0], node, seq=True)
+        self.net.add_edge(seq[1], node, seq=True)
+        node.add_edge(seq)
+
+    def get_unit_nodes(self):
+        return [n for n in self.nodes if isinstance(n, UnitNode)]
+
+    def compute_distances(self):
+        distances = {}
+        visited = set()
+        queue = []
+
+        # 初始化所有UnitNode的距离为0
+        for n in self.nodes:
+            if isinstance(n, UnitNode):
+                distances[n] = 0
+                queue.append(n)  # 将UnitNode添加到队列中，以便开始BFS
+
+        # 使用BFS计算从NonUnitNode到UnitNode的距离
+        while queue:
+            current = queue.pop(0)
+            for neighbor in self.net.successors(current):  # 获取当前节点的前驱节点
+                # if neighbor not in visited:
+                if neighbor in visited:
+                    distances[neighbor] = max(distances[current] + 1, distances[neighbor])
+                else:
+                    distances[neighbor] = distances[current] + 1
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+        return distances
+
+    def set_pos(self):
+        distances = self.compute_distances()
+        layer_counts = {}
+        for n, dist in distances.items():
+            x = dist
+            y = layer_counts.get(x, 0)
+            layer_counts[x] = y + 1
+            self.pos[n] = (x, y)
+        # 设置节点属性
+        pos_attrs = {node: {"pos": position} for node, position in self.pos.items()}
+        nx.set_node_attributes(self.net, pos_attrs)
+
+# each node is a graph, including sequence(edges) and set(nodes)
+class UnitNode:
+    id_iter = itertools.count()
+
+    def __init__(self, label=None, value=.0) -> None:
+        self.id = next(self.id_iter)
+        self.label = str(self.id) if label is None else label
+        self.value = value
+
+    def __str__(self):
+        return self.label+'-'+str(self.id)
+
+
+class NonUnitNode(UnitNode):
+# reps = [node, node...node]
+# seqs = [(node1, node2), (node3, node4)]
+    def __init__(self, reps=None, seqs=None, bias=1.0, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.bias = bias
         self.graph = nx.DiGraph()
-        self.nodes = self.graph.nodes
-        for ru in rus:
-            self.add_ru(ru)
-    def add_node(self, node_type=Type.ru, label=None, activation=0, bias=0, position=utils.get_pos(x=0)):
-        if node_type in [Type.ru, Type.pau]:
-            pass
-        elif node_type in [Type.r, Type.pa, Type.ma]:
-            pass
-        elif node_type in [Type.seq]:
-            pass
+        if reps:
+            self.graph.add_nodes_from(reps)
+        if seqs:
+            self.graph.add_edges_from(seqs)
 
+    def add_nodes(self, nodes) -> None:
+        self.graph.add_nodes_from(nodes)
 
-
-
-
+    def add_edges(self, edges) -> None:
+        self.graph.add_edges_from(edges)
--- NetImp/test.py
+++ NetImp/test.py
@@ -1,3 +1,48 @@
-'''
-Learn arithmetics from conversation
-'''
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+from Net import *
+
+# 创建Net对象
+net = Net()
+
+# 添加初始UnitNodes
+initial_unit_nodes = [UnitNode(label=f"U{i}") for i in range(30)]
+for node in initial_unit_nodes:
+    net.add_node(node)
+
+# 逐层添加NonUnitNode
+previous_nodes = initial_unit_nodes
+for i in range(1, 30):  # 10层
+    new_nodes = []
+    for _ in range(random.randint(1, 30)):  # 每层添加1到10个NonUnitNode
+        nodes_to_connect = random.sample(previous_nodes, random.choice([1, len(previous_nodes)]))
+        seqs = []
+        reps = nodes_to_connect.copy()
+        while len(reps) > 1:
+            if random.choice([True, False]):
+                chosen_seq = tuple(random.sample(reps, 2))
+                seqs.append(chosen_seq)
+                reps.remove(chosen_seq[0])
+                reps.remove(chosen_seq[1])
+            else:
+                break
+
+        non_unit_node = NonUnitNode(reps=reps, seqs=seqs)
+        net.add_node(non_unit_node)
+        new_nodes.append(non_unit_node)
+    previous_nodes = new_nodes
+
+# 设置位置
+net.set_pos()
+
+# 绘制网络
+plt.figure(figsize=(12, 8))
+node_colors = ["#303245" if isinstance(node, UnitNode) else "#F4D35E" for node in net.net.nodes()]
+edge_colors = ["#EE964B" if net.net[u][v]["seq"] else "#06D6A0" for u, v in net.net.edges()]
+edge_styles = ["dashed" if net.net[u][v]["seq"] else "solid" for u, v in net.net.edges()]
+edge_widths = [2 if net.net[u][v]["seq"] else 1 for u, v in net.net.edges()]
+
+nx.draw(net.net, pos=net.pos, with_labels=True, node_color=node_colors, edge_color=edge_colors, edge_vmin=0,
+        edge_vmax=1, width=edge_widths, style=edge_styles, node_size=300)
+plt.show()
